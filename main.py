d1323417f44565811e66b9dbe81eb1734829762e
Oleh Paduchak <oleh@i3engineering.com>
2023-05-31 18:09:58 +0300

    updated code

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import json
+import boto3
+from PIL import Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+client = boto3.client('s3')
+formats_to_export = ['png', 'bmp', 'gif']
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def file_path(path: str) -> str:
+    return f'/tmp/{path}'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def lambda_handler(event: dict, _):
+    for record in event['Records']:
+        process_record(record)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+
+def process_record(record):
+    file_key = record['s3']['object']['key']
+
+    print(f"converting {file_key}")
+
+    client.download_file(record['s3']['bucket']['name'], file_key, file_path(file_key))
+
+    with Image.open(file_path(file_key)) as image:
+        upload_formatted(file_key.split('.')[0], image)
+
+
+def upload_formatted(filename: str, image):
+    for fmt in formats_to_export:
+        full_filename = f'{filename}.{fmt}'
+        with open(file_path(full_filename), 'wb') as new_image:
+            image.save(new_image, format=fmt)
+        client.upload_file(file_path(full_filename), f'projector-assignment28-{fmt}', full_filename)
